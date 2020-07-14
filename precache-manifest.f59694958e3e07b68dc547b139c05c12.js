70af9b435c850124597ae8c31bcbc0decc6b95ab
vivek chaudhari <vivek11101997@gmail.com>
2020-07-14 17:13:58 +0530

    Updates

---
+++ precache-manifest.f59694958e3e07b68dc547b139c05c12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c18747c7321acd946545f7be7481730",
+    "url": "/city-tours/index.html"
+  },
+  {
+    "revision": "946ed8f8759a81c20474",
+    "url": "/city-tours/static/css/main.505fc90a.chunk.css"
+  },
+  {
+    "revision": "6fe8e89a67b8bab34047",
+    "url": "/city-tours/static/js/2.4ccc796c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/city-tours/static/js/2.4ccc796c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946ed8f8759a81c20474",
+    "url": "/city-tours/static/js/main.21e87e09.chunk.js"
+  },
+  {
+    "revision": "e023f412b9e7dea43a24",
+    "url": "/city-tours/static/js/runtime-main.05808f4e.js"
+  }
+]);
